bfdb03f24f8584f140ac93937df43bdd64079237
wesley22 <wesley.llamedo22@gmail.com>
2025-04-24 12:08:52 +0800

    update app bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
 </script>
 
 <template>
-  <RouterView class="" />
+  <v-layout>
+    <v-app-bar>
+      <v-app-bar-title>Neembly</v-app-bar-title>
+      <v-spacer />
+      <v-btn
+        to="/"
+        variant="text"
+        :color="route.name === 'product' || route.name === 'product' ? 'success' : undefined"
+        >Product List</v-btn
+      >
+      <v-btn
+        to="/product/crud"
+        variant="text"
+        :color="
+          route.name === 'product-crud' || route.name === 'product-crud-create'
+            ? 'success'
+            : undefined
+        "
+        >Product CRUD</v-btn
+      >
+    </v-app-bar>
+    <v-main>
+      <RouterView class="" />
+    </v-main>
+  </v-layout>
 </template>
